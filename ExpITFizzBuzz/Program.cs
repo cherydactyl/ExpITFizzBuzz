a7073d28865efebd94a952c70f09e0de4e25f2c6
cherydactyl <cheryl.orosz@gmail.com>
2015-07-09 13:52:14 -0400

    Slight Efficiency Upgrade
    Changes order of checks to improve performance

--- ExpITFizzBuzz/Program.cs
+++ ExpITFizzBuzz/Program.cs
@@ -12,13 +12,27 @@ namespace ExpITFizzBuzz
         {
             for (int i = 1; i <= 100; i++)
             {
-                if (i % 15 == 0)        // divisible by both 3 and 5 means divisible by 15
+                //if (i % 15 == 0)        // divisible by both 3 and 5 means divisible by 15
+                //{
+                //    Console.WriteLine("FizzBuzz");
+                //} else if (i % 3 == 0)  // divisible by 3
+
+                //Slightly more efficient version;
+                //checks for FizzBuzz condition only if Fizz
+                //condition is already met
+
+                if (i % 3 == 0)  // divisible by 3
                 {
-                    Console.WriteLine("FizzBuzz");
-                } else if (i % 3 == 0)  // divisible by 3
-                {
-                    Console.WriteLine("Fizz");
-                } else if (i % 5 == 0)  // divisible by 5
+                    if (i % 5 == 0)  // also divisible by 5!
+                    {
+                        Console.WriteLine("FizzBuzz");
+                    }
+                    else
+                    {
+                        // divisible by 3 but not 5
+                        Console.WriteLine("Fizz");
+                    }
+                } else if (i % 5 == 0)  // divisible by 5 (but not 3)
                 {
                     Console.WriteLine("Buzz");
                 } else                  //divisible by neither 3 nor 5
